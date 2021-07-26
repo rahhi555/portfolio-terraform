f5cf52f1383dd432a8642a5142f43627ccb23561
rahhi555 <whowhose0904@gmail.com>
2021-07-27 01:54:46 +0900

    fixed web

--- ECS.tf
+++ ECS.tf
@@ -85,65 +85,6 @@ resource "aws_ecs_cluster" "svg_portfolio" {
   name = "svg_portfolio"
 }
 
-# # ECSタスク定義 1つのタスク定義の中にwebとfront詰め込み
-# resource "aws_ecs_task_definition" "svg_portfolio" {
-#   family                   = "svg_portfolio"
-#   cpu                      = "512"
-#   memory                   = "1024"
-#   network_mode             = "awsvpc"
-#   requires_compatibilities = ["FARGATE"]
-#   # ECS ExecをするためのIAMロール
-#   task_role_arn = module.ecs_for_exec.iam_role_arn
-#   # CloudWacthにログを投げるためのタスク実行時IAMロール
-#   execution_role_arn = module.ecs_task_execution_role.iam_role_arn
-#   # コンテナ定義
-#   container_definitions = file("./container_definitions/front-web.json")
-# }
-
-# # ECSサービス
-# resource "aws_ecs_service" "svg_portfolio" {
-#   name            = "svg_portfolio_service"
-#   cluster         = aws_ecs_cluster.svg_portfolio.arn
-#   task_definition = aws_ecs_task_definition.svg_portfolio.arn
-#   desired_count   = 1
-#   launch_type     = "FARGATE"
-#   # FARGATEを選択したときのみ適用される。デフォルトは"LATEST"。
-#   platform_version                  = "1.4.0"
-#   health_check_grace_period_seconds = 60
-#   # ECS Execを有効にする
-#   enable_execute_command = true
-
-#   # awsvpcネットワークモードの際に必要になるオプション。
-#   network_configuration {
-#     # パブリックIPを割り当てるか。下のsubnetsでパブリックサブネットを割り当てるならtrueにする。
-#     # プライベートサブネットを割り当てるならfalseでいいが、その場合ecrからコンテナを引っ張ってくるため
-#     # NAT gateway 等のエンドポイントが必要になる。
-#     assign_public_ip = true
-#     security_groups  = [module.front_sg.security_group_id, module.web_sg.security_group_id]
-#     subnets = [
-#       aws_subnet.public.id,
-#     ]
-#   }
-
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.svg_portfolio.arn
-#     # コンテナ定義で設定したコンテナの名前
-#     container_name = "svg_portfolio_front"
-#     container_port = 80
-#   }
-
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.svg_portfolio_api.arn
-#     # コンテナ定義で設定したコンテナの名前
-#     container_name = "svg_portfolio_web"
-#     container_port = 3000
-#   }
-
-#   lifecycle {
-#     ignore_changes = [task_definition]
-#   }
-# }
-
 # ECSタスク定義 front
 resource "aws_ecs_task_definition" "svg_portfolio_front" {
   family = "svg_portfolio_front"
